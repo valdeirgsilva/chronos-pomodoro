7ed9e308a5ac6a3dbb325c9ac29e659a50863e4c
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-09 17:33:53 -0300

    Input: usando condicionais no JSX

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' />
+            <DefaultInput labelText='task' id='meuInput' type='text' />
           </div>
 
           <div className='formRow'>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,12 +1,18 @@
 type DefaultInputProps = {
   id: string;
+  labelText: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, type }: DefaultInputProps) {
+export function DefaultInput({
+  id,
+  type,
+  labelText,
+  ...rest
+}: DefaultInputProps) {
   return (
     <>
-      <label htmlFor={id}>task</label>
-      <input id={id} type={type} />
+      <label htmlFor={id}>{labelText}</label>
+      <input id={id} type={type} {...rest} />
     </>
   );
 }
