70708d0e3c37b7e897f1c5085c3c542e15e2309f
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-09 16:10:25 -0300

    criando o componente de CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -13,6 +14,9 @@ export function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
