1cd2f1e9bb6007b8c6acba023cfd16df22172c19
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-13 17:25:13 -0300

    limpando o código

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,13 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  // Que todos os componentes que usam "numero"
-  // saibam das mudanças em seu valor.
-
-  // Sempre que eu usar useState, não vou usar atribuição diretamente
-  // const [numero, setNumero] = useState(() => {
-  //   console.log('Lazy initialization');
-  //   return 0;
-  // });
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    // setNumero(prevState => prevState + 1);
-    setNumero(1);
-  }
-
   return (
     <>
-      <Heading>Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
       <Container>
         <Logo />
       </Container>
@@ -50,7 +30,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText={numero.toString()}
+              labelText='task'
               id='meuInput'
               type='text'
               placeholder='Digite algo'
