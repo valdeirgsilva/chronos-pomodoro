00e9804cf0282899413f39a8299c1757dfe608f6
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-13 17:21:15 -0300

    usando useState para o estado com programação reativa

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,33 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
+import { Heading } from './components/Heading';
+import { useState } from 'react';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
+  // Que todos os componentes que usam "numero"
+  // saibam das mudanças em seu valor.
+
+  // Sempre que eu usar useState, não vou usar atribuição diretamente
+  // const [numero, setNumero] = useState(() => {
+  //   console.log('Lazy initialization');
+  //   return 0;
+  // });
+  const [numero, setNumero] = useState(0);
+
+  function handleClick() {
+    // setNumero(prevState => prevState + 1);
+    setNumero(1);
+  }
+
   return (
     <>
+      <Heading>Número: {numero}</Heading>
+      <button onClick={handleClick}>Aumenta</button>
+
       <Container>
         <Logo />
       </Container>
@@ -30,7 +50,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText='task'
+              labelText={numero.toString()}
               id='meuInput'
               type='text'
               placeholder='Digite algo'
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href='#'>Entenda como funcioan a técnica pomodoro</a>
+      <a href='#'>Entenda como funciona a técnica pomodoro</a>
       <a href='#'>
         Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
       </a>
