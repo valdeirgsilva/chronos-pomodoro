5bea5f130e561d51aec27099b70cd618fd62e7d0
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-22 14:05:57 -0300

    capturando o evento submit do formulário (onSubmit com preventDefault)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,8 +4,13 @@ import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 
 export function MainForm() {
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('DEU CERTO');
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
