798970752cda2f5fb975be85b6059f601792532a
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-23 15:31:07 -0300

    adicionando a data que a tarefa foi interrompida (interruptDate)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -64,6 +64,12 @@ export function MainForm() {
         activeTask: null,
         secondsRemaining: 0,
         formattedSecondsRemaining: '00:00',
+        tasks: prevState.tasks.map(task => {
+          if (prevState.activeTask && prevState.activeTask.id === task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
       };
     });
   }
