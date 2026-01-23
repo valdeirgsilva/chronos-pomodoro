0a77f8c4b64938bdf1d0bc1ef57a9eeb579f3315
valdeirgsilva <valdeirgabriels@gmail.com>
2026-01-23 14:40:15 -0300

    feat: renderiza ciclos com classes dinâmicas e acessibilidade

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -73,9 +73,11 @@ export function MainForm() {
         <p>Próximo intervalo é de 25 min</p>
       </div>
 
-      <div className='formRow'>
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className='formRow'>
+          <Cycles />
+        </div>
+      )}
 
       <div className='formRow'>
         <DefaultButton icon={<PlayCircleIcon />} />
