88fc1de697361827a6c0f523c301975826196b92
valdeirgsilva <valdeirgabriels@gmail.com>
2026-04-23 17:43:15 -0300

    entendendo o mínimo necessário sobre Web Workers

---
+++ src/workers/timerWorker.js
@@ -0,0 +1,21 @@
+self.onmessage = function (event) {
+  console.log('WORKER recebeu:', event.data);
+
+  switch (event.data) {
+    case 'FAVOR': {
+      self.postMessage('Sim, posso fazer um favor');
+      break;
+    }
+    case 'FALA_OI': {
+      self.postMessage('OK: OI!');
+      break;
+    }
+    case 'FECHAR': {
+      self.postMessage('Tá bom, vou fechar');
+      self.close();
+      break;
+    }
+    default:
+      self.postMessage('Não entendi ');
+  }
+};
