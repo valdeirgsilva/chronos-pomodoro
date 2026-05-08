a9e12968378df69fb7dc0799da1a0c24d27b39ad
valdeirgsilva <valdeirgabriels@gmail.com>
2026-05-07 21:50:57 -0300

    web worker com setTimeout para contagem regressiva

--- src/workers/timerWorker.js
+++ src/workers/timerWorker.js
@@ -1,21 +1,25 @@
-self.onmessage = function (event) {
-  console.log('WORKER recebeu:', event.data);
+let isRunning = false;
 
-  switch (event.data) {
-    case 'FAVOR': {
-      self.postMessage('Sim, posso fazer um favor');
-      break;
-    }
-    case 'FALA_OI': {
-      self.postMessage('OK: OI!');
-      break;
-    }
-    case 'FECHAR': {
-      self.postMessage('Tá bom, vou fechar');
-      self.close();
-      break;
-    }
-    default:
-      self.postMessage('Não entendi ');
+self.onmessage = function (event) {
+  if (isRunning) return;
+
+  isRunning = true;
+
+  const state = event.data;
+  const { activeTask, secondsRemaining } = state;
+
+  const endDate = activeTask.startDate + secondsRemaining * 1000;
+  const now = Date.now();
+  let countDownSeconds = Math.ceil((endDate - now) / 1000);
+
+  function tick() {
+    self.postMessage(countDownSeconds);
+
+    const now = Date.now();
+    countDownSeconds = Math.floor((endDate - now) / 1000);
+
+    setTimeout(tick, 1000);
   }
+
+  tick();
 };
